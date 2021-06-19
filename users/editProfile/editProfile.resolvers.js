02092fb9cfd3656728c7643f8a06f38b018098c3
angelkim88 <66551410+hoharon@users.noreply.github.com>
2021-06-20 06:41:23 +0900

    function-oriented programming

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,40 +1,42 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
+import { protectedResolver } from "../user.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser, protectResolver }
-    ) => {
-      protectResolver(loggedInUser);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser, protectResolver }
+      ) => {
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could nkot update profile.",
+          };
+        }
       }
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could nkot update profile.",
-        };
-      }
-    },
+    ),
   },
 };

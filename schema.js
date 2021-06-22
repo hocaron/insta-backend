9a5a5fdcd8dc5a09dbcc702c5df480bb17d8bb98
angelkim88 <66551410+hoharon@users.noreply.github.com>
2021-06-22 15:44:24 +0900

    using altair

--- schema.js
+++ schema.js
@@ -11,9 +11,5 @@ const loadedResolvers = loadFilesSync(
   path.join(__dirname, "./**/*.resolvers.js")
 );
 
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);

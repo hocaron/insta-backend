e1eb73a9c9a35163380b47071ecb568a6283f162
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-07-01 22:52:36 +0900

    Web Socker Context 유저 인증

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -7,16 +7,21 @@ export default {
   Subscription: {
     roomUpdates: {
       subscribe: async (root, args, context, info) => {
-        const room = await client.room.findUnique({
+        const room = await client.room.findFirst({
           where: {
             id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
           },
           select: {
             id: true,
           },
         });
         if (!room) {
-          throw new Error("You shall ot see this.");
+          throw new Error("You shall not see this.");
         }
         return withFilter(
           () => pubsub.asyncIterator(NEW_MESSAGE),

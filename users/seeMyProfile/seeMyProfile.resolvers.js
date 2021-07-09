910b21b893746f3a50970912d27dcb9741a4511c
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-07-10 02:29:36 +0900

    seeMyProfile 추가

---
+++ users/seeMyProfile/seeMyProfile.resolvers.js
@@ -0,0 +1,14 @@
+import client from "../../client";
+import { protectedResolver } from "../user.utils";
+
+export default {
+  Query: {
+    seeMyProfile: protectedResolver((_, __, { loggedInUser }) => {
+      client.user.findUnique({
+        where: {
+          id: loggedInUser.id,
+        },
+      });
+    }),
+  },
+};
